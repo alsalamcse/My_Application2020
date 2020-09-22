55115165b96224f2c15b9549800a5e7cedab6ab3
samih <samih1079@gmail.com>
2020-09-22 12:58:41 +0300

    i created main activity 4,                         next i need to build GUI

--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -10,5 +10,6 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        //kjlkjlkjjljkljsdkllskjfdvlksjdfl
     }
 }
